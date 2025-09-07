1a5e0a291c88e3a156e0926157bcdc3f47c68105
AugustoCeschin <gutoceschin@hotmail.com>
2025-09-07 12:22:36 -0300

    Adicionando mais um endpoint na API e modificando a mensagem do endpoint inicial.

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ app = FastAPI()
 # http://127.0.0.1:8000/
 @app.get("/")
 def read_root():
-    return {"mensagem": "Olá Mundo!"}
+    return {"mensagem": "Olá Mundo! Bem-vindo à API utilizada na AS - DevOps"}
 
 # http://127.0.0.1:8000/primeiro-endpoint
 @app.get("/primeiro-endpoint")
 def read_root():
     return {"teste": True, "num_aleatorio": random.randint(0, 1000)}
+
+
+# http://127.0.0.1:8000/moeda
+@app.get("/moeda")
+def jogar_moeda():
+    return {"resultado": random.choice(["cara", "coroa"])}
